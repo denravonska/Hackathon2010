8bb004d0707c34443b425eb7c6892d88301b5937
Marco Alanen <marco@ormgas.com>
2010-09-11 11:03:13 +0200

    - Use the accelerometer

--- src/com/ormgas/hackathon2010/GameScene.java
+++ src/com/ormgas/hackathon2010/GameScene.java
@@ -1,31 +1,37 @@
 package com.ormgas.hackathon2010;
 
 import com.stickycoding.rokon.Scene;
+import com.stickycoding.rokon.device.Accelerometer;
 
 public class GameScene extends Scene {
 
+	AccelerometerHandler accelerometerHandler;
+	
 	@Override
 	public void onGameLoop() {
 		// TODO Auto-generated method stub
-		
+	}
+	
+	@Override
+	public void onEndScene() {
+		Accelerometer.stopListening();
+		super.onEndScene();
 	}
 
 	@Override
 	public void onPause() {
-		// TODO Auto-generated method stub
-		
+		Accelerometer.stopListening();
 	}
 
 	@Override
 	public void onReady() {
-		// TODO Auto-generated method stub
-		
+		this.accelerometerHandler = new AccelerometerHandler();
+		Accelerometer.startListening(accelerometerHandler);
 	}
 
 	@Override
 	public void onResume() {
-		// TODO Auto-generated method stub
-		
+		Accelerometer.startListening(accelerometerHandler);		
 	}
 
 }
