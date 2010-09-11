fd1cca24090d0d173353b90a04d0c18532519235
Marco Alanen <marco@ormgas.com>
2010-09-11 09:00:19 +0200

    - Added project files

---
+++ src/com/ormgas/hackathon2010/GameActivity.java
@@ -0,0 +1,13 @@
+package com.ormgas.hackathon2010;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GameActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
