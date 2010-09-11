3af6c07aa43fb115a2b009c848fbbc314831e645
Marco Alanen <marco@ormgas.com>
2010-09-11 15:06:12 +0200

    - Added AccelerometerHandler code. We should use the orientation sensor at a later stage.

--- src/com/ormgas/hackathon2010/AccelerometerHandler.java
+++ src/com/ormgas/hackathon2010/AccelerometerHandler.java
@@ -1,16 +1,25 @@
 package com.ormgas.hackathon2010;
 
+import com.stickycoding.rokon.MathHelper;
 import com.stickycoding.rokon.device.OnAccelerometerChange;
 
 public class AccelerometerHandler implements OnAccelerometerChange {
 
+	private float rotation;
+	private final static int accelMax = 10;
+	private final static float accelMult = 90f / accelMax;
+	
 	@Override
 	public void onAccelerometerChange(float x, float y, float z) {
-		// TODO Auto-generated method stub
-		
+		y = Math.min(y, AccelerometerHandler.accelMax);
+		y = Math.max(y, -AccelerometerHandler.accelMax);
+		this.rotation = MathHelper.DEG_TO_RAD * accelMult * y;
 	}
 
 	@Override
 	public void onShake(float force) {}
 
+	public float getRotation() {
+		return this.rotation;
+	}
 }
