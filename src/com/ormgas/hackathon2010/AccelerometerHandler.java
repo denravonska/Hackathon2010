4cbcc53c3b143c4daad5dfd73fefe287e5f0269c
Marco Alanen <marco@ormgas.com>
2010-09-11 09:28:21 +0200

    - Added accelerometer skeleton

---
+++ src/com/ormgas/hackathon2010/AccelerometerHandler.java
@@ -0,0 +1,16 @@
+package com.ormgas.hackathon2010;
+
+import com.stickycoding.rokon.device.OnAccelerometerChange;
+
+public class AccelerometerHandler implements OnAccelerometerChange {
+
+	@Override
+	public void onAccelerometerChange(float x, float y, float z) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onShake(float force) {}
+
+}
