fd18edbc9a126f04789629e036161d2639fd86da
Fredrik Blom <fhm.blom@gmail.com>
2010-09-11 13:42:55 +0200

    - Forgot to add the id to the FlyingObject:s.

--- src/com/ormgas/hackathon2010/gameobjects/AirplaneObject.java
+++ src/com/ormgas/hackathon2010/gameobjects/AirplaneObject.java
@@ -1,7 +1,7 @@
 package com.ormgas.hackathon2010.gameobjects;
 
 public class AirplaneObject extends FlyingObject {
-	public AirplaneObject(float x, float y, float heading) {
-		super(x, y, 1, heading, null);
+	public AirplaneObject(int id, float x, float y, float heading) {
+		super(id, x, y, 1, heading, null);
 	}
 }
--- src/com/ormgas/hackathon2010/gameobjects/BulletObject.java
+++ src/com/ormgas/hackathon2010/gameobjects/BulletObject.java
@@ -1,7 +1,7 @@
 package com.ormgas.hackathon2010.gameobjects;
 
 public class BulletObject extends FlyingObject {
-	public BulletObject(float x, float y, float heading) {
-		super(x, y, 3, heading, null);
+	public BulletObject(int id, float x, float y, float heading) {
+		super(id, x, y, 3, heading, null);
 	}
 }
