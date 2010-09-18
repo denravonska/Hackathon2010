6ed5a3488be89b795a7e5394ba3dda5d8026d119
Marco Alanen <marco@ormgas.com>
2010-09-18 11:10:45 +0200

    - Added ScrollableParallaxBackground which is affected by the camera position in addition to the parallax value.

---
+++ src/com/ormgas/hackathon2010/ScrollableParallaxBackground.java
@@ -0,0 +1,22 @@
+package com.ormgas.hackathon2010;
+
+import javax.microedition.khronos.opengles.GL10;
+
+import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.entity.scene.background.ParallaxBackground;
+import org.anddev.andengine.opengl.util.GLHelper;
+
+public class ScrollableParallaxBackground extends ParallaxBackground
+{
+	public ScrollableParallaxBackground(float pRed, float pGreen, float pBlue) {
+	        super(pRed, pGreen, pBlue);
+	        // TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void onDraw(final GL10 pGL, final Camera pCamera) {
+	        pCamera.onApplyMatrix(pGL);
+	        GLHelper.setModelViewIdentityMatrix(pGL);
+	        super.onDraw(pGL, pCamera);
+	}
+}
