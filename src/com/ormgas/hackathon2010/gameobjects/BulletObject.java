3aab0e1fb2bbb535a961124eff8bb91f21c593f0
Niblit_lvl50 <niklas.damberg@gmail.com>
2010-09-20 21:42:51 +0200

    Fixed so the explosion happens in the center of the sprite, and enlarged it even more

--- src/com/ormgas/hackathon2010/gameobjects/BulletObject.java
+++ src/com/ormgas/hackathon2010/gameobjects/BulletObject.java
@@ -8,7 +8,7 @@ public class BulletObject extends GameObject {
 
 	public BulletObject(int id, int ownerId, float x, float y, float heading) {
 		super(id, x, y, 3, heading, Textures.bullet);
-		this.setScale(2f, 2f);
+		this.setScale(3f, 3f);
 	}
 	
 	@Override
@@ -22,9 +22,9 @@ public class BulletObject extends GameObject {
 			try
 			{
 				ExplosionObject explosion = ObjectHandler.obtainItem(ExplosionObject.class);
-				explosion.setPosition(this.mX, this.mY);
+				explosion.setPosition(x - (explosion.getWidth() / 2), y - (explosion.getHeight() / 2));
 				explosion.animate(70, false, explosion);
-				Sounds.explosion2.play();
+				Sounds.explosion1.play();
 
 				ObjectHandler.recyclePoolItem(this);
 			}
