a30a76cd2a629c92f7afd119074ab1eddf93d7ac
Niklas Damberg <niklas.damberg@gmail.com>
2010-09-24 18:33:18 +0200

    MachineGun again, missing on repo

--- src/com/ormgas/hackathon2010/weapons/MachineGun.java
+++ src/com/ormgas/hackathon2010/weapons/MachineGun.java
@@ -1,13 +1,10 @@
 package com.ormgas.hackathon2010.weapons;
 
-import android.util.Log;
 
 import com.ormgas.hackathon2010.Sounds;
 import com.ormgas.hackathon2010.eventbus.EventBus;
 import com.ormgas.hackathon2010.eventbus.SpawnBulletEvent;
-import com.ormgas.hackathon2010.gameobjects.BulletObject;
 import com.ormgas.hackathon2010.gameobjects.GameObject;
-import com.ormgas.hackathon2010.gameobjects.ObjectHandler;
 
 public class MachineGun implements IWeapon
 {
@@ -34,26 +31,11 @@ public class MachineGun implements IWeapon
 	
 	private void doFire()
 	{
-		try
-		{
-			//BulletObject bullet = ObjectHandler.obtainItem(BulletObject.class);
-			//bullet.setParentId(parent.getId());
-			//bullet.setPosition(parent.getX() + parent.getWidth() / 2, parent.getY() + parent.getHeight() / 2);
-			//bullet.setRotation(parent.getRotation());
-			//bullet.setVelocity(projectileVelocity);
-		
-			//bullet.setVelocity(parent.getVelocityX() * 5, parent.getVelocityY() * 5);
-			
-			final float x = parent.getX() + (parent.getWidth() / 2);
-			final float y = parent.getY() + (parent.getHeight() / 2);
-			final float velocityX = parent.getVelocityX() * 5;
-			final float velocityY = parent.getVelocityY() * 5;
-			EventBus.instance().dispatch(new SpawnBulletEvent(parent.getId(), x, y, velocityX, velocityY));
-		}
-		catch(Exception e)
-		{
-			Log.d(TAG, "Caught exception in doFire(): " + e);
-		}
+		final float x = parent.getX() + (parent.getWidth() / 2);
+		final float y = parent.getY() + (parent.getHeight() / 2);
+		final float velocityX = parent.getVelocityX() * 5;
+		final float velocityY = parent.getVelocityY() * 5;
+		EventBus.instance().dispatch(new SpawnBulletEvent(parent.getId(), x, y, velocityX, velocityY));
 		
 		Sounds.shoot.play();
 		fireTimer = System.currentTimeMillis();
