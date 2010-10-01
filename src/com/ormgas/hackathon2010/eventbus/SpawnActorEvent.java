26eef5eb2ec30111e483cf6d1cb483d6834df28e
Marco Alanen <marco@ormgas.com>
2010-10-01 14:20:35 +0200

    - Added empty constructor

--- src/com/ormgas/hackathon2010/eventbus/SpawnActorEvent.java
+++ src/com/ormgas/hackathon2010/eventbus/SpawnActorEvent.java
@@ -5,13 +5,15 @@ import com.ormgas.hackathon2010.controller.IGameObjectController;
 public class SpawnActorEvent {
 	public int actorId;
 	public IGameObjectController controller;
-	public boolean isLocalActor;
-	
+	public boolean isLocalActor;	
 	
 	public SpawnActorEvent(int actorId, IGameObjectController controller, boolean isLocalActor) {
 		this.actorId = actorId;
 		this.controller = controller;
 		this.isLocalActor = isLocalActor;
+	}
+	
+	public SpawnActorEvent() {
 	}
 	
 	public void set(int actorId, IGameObjectController controller, boolean isLocalActor) {
