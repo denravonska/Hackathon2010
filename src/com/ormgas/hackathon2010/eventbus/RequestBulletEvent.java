c8a45409b522579861b139ec0d966070539d8ea4
Marco Alanen <marco@ormgas.com>
2010-10-07 18:27:51 +0200

    - RequestBulletEvent is now an IRequestEvent

--- src/com/ormgas/hackathon2010/eventbus/RequestBulletEvent.java
+++ src/com/ormgas/hackathon2010/eventbus/RequestBulletEvent.java
@@ -2,7 +2,9 @@ package com.ormgas.hackathon2010.eventbus;
 
 import java.io.Serializable;
 
-public class RequestBulletEvent implements Serializable {
+import com.ormgas.hackathon2010.gameobjects.ObjectHandler;
+
+public class RequestBulletEvent implements Serializable, IRequestEvent {
 	private static final long serialVersionUID = 1L;
 	public int id = 0;
 	public float x = 0;
@@ -21,5 +23,12 @@ public class RequestBulletEvent implements Serializable {
 		this.velocityX = velocityX;
 		this.velocityY = velocityY;
 		this.rotation = rotation;
+	}
+
+	@Override
+	public Object createResponse() {		
+		SpawnBulletEvent event = ObjectHandler.obtainItem(SpawnBulletEvent.class);
+		event.set(this.id, this.x, this.y, this.velocityX, this.velocityY, this.rotation);
+		return event;
 	}
 }
